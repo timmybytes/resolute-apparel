e6f349e3113103f03e14864946da64ba3ba99e74
Timothy Merritt <timmybytes@gmail.com>
2020-11-28 12:17:37 -0800

    (feat): Add basic routing and custom favicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import HomePage from '../pages/homepage/HomePage';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </>
   );
 }
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Directory from '../../components/Directory';
 
 const HomePage = () => (
-  <div className="homepage">
+  <div className='homepage'>
     <Directory />
   </div>
 );
