0d55da6a6c1500af5cdcb85c804144f141502cea
Timothy Merritt <timmybytes@gmail.com>
2020-11-28 11:43:15 -0800

    (feat): Add font, bezier scaling effect

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className={`${size} menu-item`}>
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
 );
