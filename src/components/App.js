2d4baa83112c7f51f3c0c637d57f3b5fd1df7a4c
Timothy Merritt <timmybytes@gmail.com>
2020-11-29 13:59:30 -0800

    (feat): Create custom button

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
 import { Route, Switch } from 'react-router-dom';
+import Header from './Header';
+import SignInPage from '../pages/SignInPage';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInPage} />
       </Switch>
     </>
   );
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, imgUrl, size, history, linkUrl, match }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
       style={{
-        backgroundImage: `url(${imgUrl})`,
+        backgroundImage: `url(${imageUrl})`,
       }}
     />
     <div className='content'>
