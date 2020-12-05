81ca5d369300b5e272fe26a91fd7eb086fd7c80f
Timothy Merritt <timmybytes@gmail.com>
2020-12-04 20:05:54 -0800

    (feat): Add basic Redux config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Resolute Apparel Site
 
-This is a React eCommerce site WIP
+This is a React mock eCommerce site. It's built with React, Redux, SCSS, and Firebase for the backend.
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './components/App';
 import './scss/main.scss';
 
 ReactDOM.render(
-  <BrowserRouter basename='/'>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename='/'>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
