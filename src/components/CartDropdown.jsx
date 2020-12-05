db4b513b347257c87acf1d1d5b0f2bd9590381a5
Timothy Merritt <timmybytes@gmail.com>
2020-12-05 15:41:25 -0800

    (feat): Create CartItem component, cart dropdown

--- src/components/CartDropdown.jsx
+++ src/components/CartDropdown.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from './CustomButton';
+import CartItem from './CartItem';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='cart-item__details'>
+      <span className='cart-item__details-name'>{name}</span>
+      <span className='cart-item__details-price'>
+        {quantity} x ${price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem;
