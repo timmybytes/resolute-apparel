4e2fa79ad4c12594ded82dbe28ccc492e4095ad0
Timothy Merritt <timmybytes@gmail.com>
2020-12-06 09:41:21 -0800

    (feat): Add support for Redux Firefox extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,10 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
