517fbb76ec40eab7c98c12bd34d51bbfc3ff8dda
Timothy Merritt <timmybytes@gmail.com>
2020-12-07 07:49:52 -0800

    (feat): Add shop to redux, convert classes to functions

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import SHOP_DATA from './shop-data';
-import CollectionPreview from '../components/CollectionPreview';
+import CollectionsOverview from '../components/CollectionsOverview';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
